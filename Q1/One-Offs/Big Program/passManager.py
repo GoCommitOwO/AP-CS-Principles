7dceddbfffbb3d8c9aaf71c98da822bbd48ad116
GoCommitOwO <rynes.a@stu.janesville.k12.wi.us>
2023-11-01 09:35:50 -0500

    what the hell is this

--- Q1/One-Offs/Big Program/passManager.py
+++ Q1/One-Offs/Big Program/passManager.py
@@ -7,37 +7,71 @@ import rsa
 #private key to decrypt the password
 
 # Read the RSA key from a text file, catch the error if the file is not found
-with open("key.txt", "r") as key_file:
-    rsa_key = key_file.read()
-    try:
-        rsa_key = rsa.PrivateKey.load_pkcs1(rsa_key)
-        print("RSA key loaded successfully.")
-        print("Your RSA key is: ", str(rsa_key))
-    except:
-        print("Unable to load RSA key. Check for the key.txt file in the same directory as this program.")
+try:
+    with open ('keys/privkey.pem', 'rb') as f:
+        #first encode the key to bytes, then load it
+        privkeyplaintext = f.read().decode()
+        privkey = rsa.PrivateKey.load_pkcs1(privkeyplaintext)
+    with open ('keys/pubkey.pem', 'rb') as m:
+        pubkeyplaintext = m.read().decode()
+        pubkey = rsa.PublicKey.load_pkcs1(pubkeyplaintext)
+
+except FileNotFoundError:
+    print("RSA keys not found. They should be in the 'keys' folder.")
+    print("Enter 1 to generate new keys, or 2 to exit.")
+    choice = input("Choice: ")
+    if choice == '1':
+        #generate the keys
+        pubkey, privkey = rsa.newkeys(512)
+
+        #write the keys to the file
+        with open ('keys/privkey.pem', 'wb') as f:
+            f.write(privkey.save_pkcs1('PEM'))
+        with open ('keys/pubkey.pem', 'wb') as f:
+            f.write(pubkey.save_pkcs1('PEM'))
+
+        print("RSA keys generated and saved in the 'keys' folder.")
+        print("Deleting current logins.txt file, as the keys have changed.")
+        try:
+            os.remove('logins.txt')
+            print("logins.txt file deleted.")
+        except FileNotFoundError:
+            print("No logins.txt file found, one will get generated at the next step.")
+    else:
         exit()
 
+#import a "datasheet" dictionary variable from logins.txt, or create a new one if the file is not found
+try:
+    with open('logins.txt', 'rb') as f:
+        datasheet = eval(f.read())
+        print("Logins.txt file loaded!.")
+except FileNotFoundError:
+    datasheet = {}
+    print("No logins.txt file found. Creating a new one.")
 
-# Initialize an empty list to store account-password pairs
-datasheet = {}
 
 def add_password(account, password):
     account = string.capwords(account)
     if account in datasheet:
         print(f"Account '{account}' already exists.")
         return
-    datasheet[account] = password
+    #encrypt the password
+    password = rsa.encrypt(password.encode(), pubkey)
+    #write the encrypted password to the file
+    with open('logins.txt', 'wb') as f:
+        f.write(str(datasheet).encode())
+
 
 def get_password(account):
     account = string.capwords(account)
     if account in datasheet:
-        return datasheet[account]
+        return rsa.decrypt(datasheet[account], privkey).decode()
     else:
         return False
 
 def list_accounts():
     if not datasheet:
-        print("Password manager is empty.")
+        print("Password manager is currently empty.")
         return
 
     print("Stored accounts:")
