bfa612a312672fdfa90db7acc3b5f6864fafbb25
Andrew :D <80910571+GoCommitOwO@users.noreply.github.com>
2024-04-17 10:29:07 -0500

    Update Longest_Substring_Without_Repeating_Characters.py

--- Marquette/Longest_Substring_Without_Repeating_Characters.py
+++ Marquette/Longest_Substring_Without_Repeating_Characters.py
@@ -1,34 +1,20 @@
-#The function should return the number of characters in the largetst substring
-#without a repeating character
-
-#Ex. "abcacbbb -> 3 "abc or "bbb" -> 1 "b
-
-#Input: @param: string -> You enter a string and find the largest subtring
-
-#Output: Integer - The number of characters in the largest substring
-
 def lengthOfLongestSubstring(s):
     maxLength = 0
-    #TODO: Fill an array with the characters of the string
-    array = ([*s])
-    #TODO: Find the longest substring within array
-    print(array)
-    #Return number of characters within substring
+    start = 0
+    charIndex = {}
+
+    for end in range(len(s)):
+        if s[end] in charIndex:
+            start = max(start, charIndex[s[end]] + 1)
+        charIndex[s[end]] = end
+        maxLength = max(maxLength, end - start + 1)
 
     return maxLength
 
-
-
-
-
-#It is not necessary for you to change the main.
-#The main was provided to make sure it conforms
-#to the input and output requirenmetns.
-
 def main():
-    input_str = input()
+    input_str = input("Enter a string: ")
     answer = lengthOfLongestSubstring(input_str)
-    print(answer)
+    print("Length of longest substring without repeating characters:", answer)
 
 if __name__ == "__main__":
     main()
